876c72600211ded03618ba3893bf8f198f1a90be
ZachoDev <zachoproject@gmail.com>
2024-11-19 01:01:39 -0400

    Changed pixel bottom header

--- layouts/default.vue
+++ layouts/default.vue
@@ -70,7 +70,7 @@ function isOutsideClicked(event) {
             </div>
         </div>
 
-        <div class="container text-center mx-auto mt-10 px-[1rem] lg:px-auto 2xl:px-[20rem]">
+        <div class="container text-center mx-auto mt-20 px-[1rem] lg:px-auto 2xl:px-[20rem]">
             <div>
                 <slot />
             </div>
